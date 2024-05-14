6a54bb6c2e201286bca17bd27d72cf08d20b4f7d
douglas woodward <doug@fourthpin.com>
2024-05-13 21:33:48 -0700

    adds a svelte web page with svelte cubed

---
+++ fourthpin.com/src/cubes.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from 'svelte';
+  import { useActions } from 'svelte-cubed';
+
+  let cube;
+
+  onMount(() => {
+    const { setRotation } = useActions(cube);
+    setInterval(() => {
+      setRotation([Math.random() * 360, Math.random() * 360, Math.random() * 360]);
+    }, 1000);
+  });
+</script>
+
+<scene>
+  <perspectiveCamera />
+  <pointLight />
+  <mesh ref={cube} geometry={cube} scale={5} />
+</scene>
